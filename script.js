ed8777763f232344c08a68e37d60264c28a85011
Anghelina-P <anghelinaproskuryakova@gmail.com>
2025-09-24 13:26:31 +0300

    сделала задания по js

---
+++ script.js
@@ -0,0 +1,90 @@
+/*let a = 10; 
+let b = 2;
+alert (a);
+alert (a + 10);
+let iphone = 2007;
+alert (iphone);
+let js = "Brendan Eich";
+alert (js);
+alert (a + b);
+alert (a - b);
+alert (a * b);
+alert (a / b);
+let result = b**5;
+alert (result);
+a--;
+let count = a % b;
+alert (count);
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num++;
+num--;
+alert(num);
+let age = prompt (`Сколько вам лет?`);
+alert (age);
+let user = {
+   name: 'Катя',
+   age: 20,
+   isAdmin: true
+};
+let newName = prompt ('Ведите ваше имя');
+alert (`Привет,${newName}!`);
+*/
+
+
+
+
+ let password = 'пароль';
+prompt ('Введите пароль');
+if (password = 'пароль') {
+   console.log('Пароль введен верно');
+} else {
+   console.log("Пароль введен неправильно");
+}
+
+let c = Number(prompt('Введите число от 0 до 10'));
+c > 0 && c < 10 ? console.log('Верно') : console.log('Неверно');
+
+   let d = 66;
+   let e = 277;
+   if (e>100||d>100) {
+      console.log('Верно');
+   } else {
+    console.log('Неверно');
+   }
+
+   let a = "2";
+   let b = "3";
+   let numbA = Number(a);
+   let numbB = Number(b);
+   console.log(numbA + numbB);
+
+   let monthNumber = Number(prompt('Введите номер месяца, чтобы узнать к какому сезону он принадлежит'));
+   switch (monthNumber) {
+     case 12:
+     case 1:
+     case 2:
+        console.log('Зима');
+        break;
+     case 3:
+     case 4:
+     case 5:
+        console.log('Весна');
+        break;
+     case 6:
+     case 7:
+     case 8:
+        console.log('Лето');
+        break;
+     case 9:
+     case 10:
+     case 11:
+        console.log('Осень');
+        break;
+     default:
+        console.log('Месяца с таким номером нет');
+        break;
+}
