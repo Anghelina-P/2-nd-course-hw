2a3d7aa74229f5e74585656c2674379edb8181d3
Anghelina-P <anghelinaproskuryakova@gmail.com>
2025-10-13 19:33:07 +0300

    сделала домашку 2.5

--- script.js
+++ script.js
@@ -1,138 +1,128 @@
-/*let a = 10; 
-let b = 2;
-alert (a);
-alert (a + 10);
-let iphone = 2007;
-alert (iphone);
-let js = "Brendan Eich";
-alert (js);
-alert (a + b);
-alert (a - b);
-alert (a * b);
-alert (a / b);
-let result = b**5;
-alert (result);
-a--;
-let count = a % b;
-alert (count);
-let num = 1;
-num += 5;
-num -= 3;
-num *= 7;
-num /= 3;
-num++;
-num--;
-alert(num);
-let age = prompt (`Сколько вам лет?`);
-alert (age);
-let user = {
-   name: 'Катя',
-   age: 20,
-   isAdmin: true
-};
-let newName = prompt ('Ведите ваше имя');
-alert (`Привет,${newName}!`);
-*/
 
 
+function guessNumGame(userNum) {
 
-/*
- let password = 'пароль';
-prompt ('Введите пароль');
-if (password = 'пароль') {
-   console.log('Пароль введен верно');
-} else {
-   console.log("Пароль введен неправильно");
-}
+   let attemps = 0;
 
-let c = Number(prompt('Введите число от 0 до 10'));
-c > 0 && c < 10 ? console.log('Верно') : console.log('Неверно');
+   let secretNum =  Math.floor(Math.random() * 100) + 1;
+   
+   do {
 
-   let d = 66;
-   let e = 277;
-   if (e>100||d>100) {
-      console.log('Верно');
-   } else {
-    console.log('Неверно');
+      userNum = Number(prompt('Попытайся угадать число от 1 до 100'));
+      attemps++;
+
+   if (userNum<secretNum) {
+       alert('Твоё число меньше загаданного');
    }
-
-   let a = "2";
-   let b = "3";
-   let numbA = Number(a);
-   let numbB = Number(b);
-   console.log(numbA + numbB);
-
-   let monthNumber = Number(prompt('Введите номер месяца, чтобы узнать к какому сезону он принадлежит'));
-   switch (monthNumber) {
-     case 12:
-     case 1:
-     case 2:
-        console.log('Зима');
-        break;
-     case 3:
-     case 4:
-     case 5:
-        console.log('Весна');
-        break;
-     case 6:
-     case 7:
-     case 8:
-        console.log('Лето');
-        break;
-     case 9:
-     case 10:
-     case 11:
-        console.log('Осень');
-        break;
-     default:
-        console.log('Месяца с таким номером нет');
-        break;
-}
-*/
-/*
-let i = 1;
-let c = 2;
-while (i<=c) {
-    console.log('Привет!');
-    i++;
+   else if (userNum > secretNum) {
+      alert('Твоё число больше загаданного');
+   } 
+   else {
+       alert('Поздравляю, ты угадал');
+   }
+      
+   } while (userNum!==secretNum);
 }
 
-let a = 1;
-while (a <= 5) {
-    console.log(a);
-    a++;    
+
+
+
+function minNum (a, b) {
+
+   if (a<b) {
+      return a;
+   }
+   return b;
+}
+let result = minNum (8, 4);
+console.log(result);
+result = minNum (6, 6);
+console.log(result);
+
+
+
+function evenNum(n) {
+
+   return Number(n) % 2 === 0 ? 'Число четное' : 'Число нечетное';
+}
+let n = prompt('Введите число');
+console.log(evenNum(n));
+
+
+
+function squareNum(c) {
+   return c*c;
 }
 
-let b = 7;
-while (b <= 22) {
-    console.log(b);
-    b++;    
+console.log(squareNum(3));
+
+
+
+let e = prompt('Сколько вам лет?')
+function ageUser(e) {
+
+   if (e<0) {
+      return 'Вы ввели неправильное значение';
+   } 
+   else if (e>0 && e<13) {
+      return 'Привет, друг!';
+   }
+   else {
+      return 'Добро пожаловать!';
+   }
+}
+console.log(ageUser(e));
+
+
+
+function mult(f, g) {
+    
+   let num1 = Number(f);
+   let num2 = Number(g);
+   
+   return (isNaN(num1) || isNaN(num2)) ? 'Одно или оба значения не являются числом' : num1 * num2;
 }
 
-let obj = {
-  "Коля": '200',
-  "Вася": '300', 
-  "Петя": '400'
-};
-for (let key in obj) {
-  console.log(`${key} — зарплата ${obj[key]} долларов`);
+let res = mult(4, 7);
+console.log(res);  
+
+res = mult('абвг', 6);
+console.log(res);
+
+
+
+let h = prompt('Введите число');
+function cubedNum(h) {
+
+   let num = Number(h);
+
+   return (isNaN(num)) ? 'Переданный параметр не является числом' : `${num} в кубе равняется ${num*num*num}`;
 }
+console.log(cubedNum(h));
 
-let n = 1000;
-let num = 0;
-do {
-    n = n / 2;
-    num++;
-} while (n >= 50);
-console.log("Финальное число:", n);
-console.log("Количество итераций:", num);
 
-let friday = 5;
-do {
-    console.log(`Сегодня пятница, ${friday}-е число. Необходимо подготовить отчет.`);
-    friday += 7; 
-} while (friday <= 31); */
 
-let guessNumber = (prompt('Угадайте число от 1 до 100')) => {
-
+let circle1 = {
+   radius: 20,
+   getArea() {
+      return `Площадь первого круга равна ${this.radius*this.radius*3.14}`;
+   },
+   getPerimeter() {
+      return `Периметр первого круга равен ${2*3.14*this.radius}`;
+   }
 }
+console.log(circle1.getArea());
+console.log(circle1.getPerimeter());
+
+let circle2 = {
+   radius: 15,
+   getArea() {
+      return `Площадь второго круга равна ${this.radius*this.radius*3.14}`;
+   },
+   getPerimeter() {
+      return `Периметр второго круга равен ${2*3.14*this.radius}`;
+   }
+   
+}
+console.log(circle2.getArea());
+console.log(circle2.getPerimeter());
